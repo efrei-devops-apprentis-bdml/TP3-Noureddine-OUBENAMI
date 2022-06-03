2bb5bf75770f8e0b9eb402b2387f1c3c572e4c60
NordineOub <75072085+NordineOub@users.noreply.github.com>
2022-06-03 15:12:01 +0200

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,10 +1,11 @@
 
 import requests
 import json
-from pprint import pprint
+import os
+from pprint import pprint #Affichage améliorée du format JSO
 
 def get_meteo(lat,lon,api_key):
-
+    #utilisation de l'API OpenWeather Map
     url = "https://api.openweathermap.org/data/2.5/onecall?lat=%s&lon=%s&appid=%s&units=metric" % (lat, lon, api_key)
 
     weather_data = requests.get(url).json()
@@ -19,6 +20,10 @@ def get_meteo(lat,lon,api_key):
     print('Latitude : ', resp['lat'], '  Longitude : ' ,resp['lon'], ' zone : ', resp['timezone'] )
     print('\nTempérature :')
     print (resp['current'] )
+# Récupération des variables d'environnement
+latitude = os.environ['LAT']
+longitude = os.environ['LONG']
+cle_api = os.environ['API_KEY']
 
 
-get_meteo(lat,lon, api_key)
+get_meteo(latitude,longitude, cle_api)
